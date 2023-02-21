a015168eea622bd323fe975a5b86523f64975ce6
Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
2023-02-21 14:12:08 +0530

    update cert path
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -19,8 +19,8 @@ import (
 // default values for config
 var (
 	defaultOrphanedResourceDuration = 5 * time.Minute
-	defCertPath                     = "tls.crt"
-	defKeyPath                      = "tls.key"
+	defCertPath                     = "../../tls.crt"
+	defKeyPath                      = "../../tls.key"
 )
 
 // External config values
