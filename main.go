25178eff542023f6d8bc3c8e55dda960464bf2d4
Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
2023-02-21 13:09:40 +0530

    update main package
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -23,17 +24,22 @@ func init() {
 }
 
 func main() {
+	log.Println("Inside main")
+	defer log.Println("Exit main")
 	// created a tickerForOrphanedResource which continues to tick after every 1 minute
+	log.Println("1")
 	tickerForOrphanedResource := time.NewTicker(config.OrphanedResourceDuration)
 	defer tickerForOrphanedResource.Stop()
 
 	// channel to mark completion
 	done := make(chan bool)
 
+	log.Println("2")
 	// await termination signals from OS on a channel
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
 
+	log.Println("3")
 	http.Handle("/metrics", promhttp.Handler())
 
 	// readiness check
@@ -43,18 +49,32 @@ func main() {
 	// start runner in a separate goroutine which
 	// listens for either tick from the ticker or a a signal to stop.
 	// go runner(ticker, done)
+	log.Println("4")
+	fmt.Println("tickerForOrphanedResource: ", *tickerForOrphanedResource)
+	fmt.Println("done: ", done)
 	go runnerForOrphanedRes(tickerForOrphanedResource, done)
+
+	// blocks the main goroutine infinitely until user terminates the process
+	sig := <-shutdown
+	done <- true
+	log.Printf("received %s, terminating application", sig)
+	close(shutdown)
+	close(done)
 }
 
 func runnerForOrphanedRes(ticker *time.Ticker, done <-chan bool) {
+	fmt.Println("Inside runnerForOrphanedRes ")
+	defer fmt.Println("Exit runnerForOrphanedRes")
 	for {
 		select {
 		case <-done:
+			fmt.Println("RETURN")
 			return
 
 		// the countorphanedresource.SendOrphanedResourceMetricsAlertInfo()
 		// function will get triggered after every 1 minute
 		case <-ticker.C:
+			fmt.Println("TICKER")
 			errSendCountOrphanedResourceAlert := orphanresources.SendOrphanedResourceMetricsAlertInfo()
 			if errSendCountOrphanedResourceAlert != nil {
 				log.Println("error while sending Orphaned Resource metric", errSendCountOrphanedResourceAlert)
